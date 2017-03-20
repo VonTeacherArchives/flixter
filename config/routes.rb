9c5002d15cb85f91fa59d184e60470e7dc66e4c6
Vaughn Weiss <vaughn.weiss@gmail.com>
2017-03-20 23:01:00 +0000

    Add new lesson modal to instructor course page

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     resources :lessons, only: :update
     resources :sections, only: :update
     resources :sections, only: [] do
-      resources :lessons, only: [:new, :create]
+      resources :lessons, only: :create
     end
     resources :courses, only: [:new, :create, :show, :edit, :update] do
       resources :sections, only: :create
