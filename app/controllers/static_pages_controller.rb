cdb93eebf26a50197c463574fde281c9fa041f30
VonTeacher <vaughn.weiss@gmail.com>
2017-03-24 20:50:47 +0000

    Configure static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,13 @@ class StaticPagesController < ApplicationController
   def index
   end
 
+  def careers
+  end
+
+  def privacy
+  end
+
+  def team
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   devise_for :users
-
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'team', to: 'static_pages#team'
+  get 'careers', to: 'static_pages#careers'
 
   resource :dashboard, only: :show
 
